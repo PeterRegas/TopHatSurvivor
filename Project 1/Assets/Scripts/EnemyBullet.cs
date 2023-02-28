947a0e6ce3aba0afccc37b243ebb1af26987b05b
PeterRegas <55000513+PeterRegas@users.noreply.github.com>
2023-02-28 15:58:17 -0500

    Massive shooting overhaul tons of bugs fixed as well

--- Project 1/Assets/Scripts/EnemyBullet.cs
+++ Project 1/Assets/Scripts/EnemyBullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
-    public float shotSpeed = 0.1f;
+    public int shotSpeed = 10;
     public int range = 1;
     public int damage = 5;
     public Rigidbody2D bulletPhysics;
@@ -12,16 +12,16 @@ public class EnemyBullet : MonoBehaviour
     void Start()
     {
         Destroy(gameObject,range);
-         Vector2 shotDir = Random.insideUnitCircle.normalized* 2;
-         shotDir = shotDir/shotDir.magnitude;
-         bulletPhysics.rotation = Mathf.Atan2(shotDir.y, shotDir.x) * Mathf.Rad2Deg;
-         bulletPhysics.velocity = shotDir * shotSpeed;
+        Vector2 shotDir = Random.insideUnitCircle;
+        shotDir.Normalize();
+        bulletPhysics.rotation = Mathf.Atan2(shotDir.y, shotDir.x) * Mathf.Rad2Deg;
+        bulletPhysics.velocity = shotDir * shotSpeed;
         
     }
     void OnTriggerEnter2D(Collider2D thing)
     {
         if(thing.tag == "Player"){
-            thing.GetComponent<PlayerAttributes>().updateHealth(-10);
+            thing.GetComponent<PlayerAttributes>().updateHealth(-damage);
             Destroy(gameObject);
         }
     }
