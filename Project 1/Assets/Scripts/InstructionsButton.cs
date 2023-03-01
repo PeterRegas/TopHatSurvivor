c9ec92fd093b9d91f98fe145dd8b5461a65a6088
Jonathan Zhu <jzhu257@gmail.com>
2023-02-28 19:40:28 -0500

    Next Page button backend

--- Project 1/Assets/Scripts/InstructionsButton.cs
+++ Project 1/Assets/Scripts/InstructionsButton.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class InstructionsButton : MonoBehaviour
 {
+    private GameObject page1;
 
+    private void Awake()
+    {
+        page1 = GameObject.Find("Page1");
+    }
     public void goToMainMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
+    public void goNextPage()
+    {
+        page1.SetActive(false);
+    }
+    public void goPrevPage()
+    {
 
+    }
 }
