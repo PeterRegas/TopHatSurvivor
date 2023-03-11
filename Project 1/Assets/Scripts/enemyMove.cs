ef65d2bd1e338ebe20bdecaf6a73337e8b29d035
PeterRegas <55000513+PeterRegas@users.noreply.github.com>
2023-03-10 21:35:03 -0500

    comment enemy move script

--- Project 1/Assets/Scripts/enemyMove.cs
+++ Project 1/Assets/Scripts/enemyMove.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class enemyMove : MonoBehaviour
 {
+    //enemy move speed
     public float movementSpeed = 8f;
+    //sprite for the enemy for animations
     [SerializeField] private SpriteRenderer sprite;
+    //rigid body for physics
     public Rigidbody2D enemyPhysics;
     Vector2 move;
+    //player so it can move towards them
     public Transform targplayer;
     // Start is called before the first frame update
     void Start()
     {
+        //finds the player
         targplayer = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {       
+        //walks towards the player
         Vector3 direction = (targplayer.position - transform.position).normalized;
         move = direction;
+        //flip sprite to face player
         if(direction.x<0){
             sprite.flipX=true;
         }
