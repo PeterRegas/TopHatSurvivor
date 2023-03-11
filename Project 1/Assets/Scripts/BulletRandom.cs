1f16bed8b7ab43fc3d0f6db9c5a21ca19cb6121e
PeterRegas <55000513+PeterRegas@users.noreply.github.com>
2023-03-10 21:33:22 -0500

    comment the random bullet script

--- Project 1/Assets/Scripts/BulletRandom.cs
+++ Project 1/Assets/Scripts/BulletRandom.cs
@@ -12,16 +12,20 @@ public class BulletRandom : MonoBehaviour
     void Start()
     {
         Destroy(gameObject,range);
+        //picks random direction
         Vector2 shotDir = Random.insideUnitCircle;
         shotDir.Normalize();
+        //rotates bullet to the direction its being shot
         bulletPhysics.rotation = Mathf.Atan2(shotDir.y, shotDir.x) * Mathf.Rad2Deg;
+        //changes the speed of the bullet to the shotspeed
         bulletPhysics.velocity = shotDir * shotSpeed;
-
+        //changes damage based off player level
         damage += GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttributes>().level*1.2f;
     }
 
     void OnTriggerEnter2D(Collider2D thing)
     {
+        //if bullet hits enemy do damage
         if(thing.tag == "enemy"){
             thing.GetComponent<enemy>().Hit(damage);
             Destroy(gameObject);
