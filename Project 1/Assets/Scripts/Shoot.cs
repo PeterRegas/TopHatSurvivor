a15397964f0b080acd1d1844a2818f10ff9fb3e8
PeterRegas <55000513+PeterRegas@users.noreply.github.com>
2023-03-10 21:28:52 -0500

    comment shoot

--- Project 1/Assets/Scripts/Shoot.cs
+++ Project 1/Assets/Scripts/Shoot.cs
@@ -6,8 +6,11 @@ public class Shoot : MonoBehaviour
 {
     //where the bullet fires from
     public Transform shootSpot;
+    //bullet prefab for standard shot
     public GameObject Bullet;
+    //random direction bullet prefab for when player hits level 5
     public GameObject BulletRandom;
+    //fire rate of bullets
     public float fireRate;
 
     public float baseFire;
@@ -21,9 +24,11 @@ public class Shoot : MonoBehaviour
     {
         i++;
         j++;
+        //increase firerate based on level
         if(gameObject.tag == "Player" & fireRate >= 0 &GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttributes>().level>1){
             fireRate = baseFire - (6*GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttributes>().level);
         }
+        //fire a random direction shot once every 5 shots after level 4
         if(gameObject.tag == "Player" & GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttributes>().level>4 & j>=fireRate*5){
             FireRandom();
             j=0;
@@ -35,9 +40,11 @@ public class Shoot : MonoBehaviour
         
     }
     void Fire(){
+        //create the bullet
         Instantiate(Bullet, shootSpot.position, shootSpot.rotation);
     }
     void FireRandom(){
+        //create the random bullet
         Instantiate(BulletRandom, shootSpot.position, shootSpot.rotation);
     }
 }
